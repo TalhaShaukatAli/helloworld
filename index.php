de3b4643cc4ff3a74b80179f8e5ad928c26ef0ad
TalhaShaukatAli <tr7251ub@go.minnstate.edu>
2024-01-10 19:37:32 -0600

    modified the code

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$name= "MyName";
+$name= "Talha";
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +10,30 @@ $name= "MyName";
     <title>Hello World</title>
 </head>
 <body>
-    <h1>Hellooo <?php echo $name ?>Talha</h1>
+    <h1>Hellooo <?php echo $name ?></h1>
+    <!--Navigation---->
+    <nav>
+        <ul>
+            <li> <a href="index.php"> Home</a></li>
+            <li> <a href="assignments.php"> Assignments</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <div>
+            <h3> Question 1: Why are you taking this class? </h3>
+            <p></p>
+            <p></p>
+        </div>
+
+        <div>
+            <h3> Question 2: What do you want to take away from this class? </h3>
+            <p></p>
+        </div>
+
+    </main>
+
+    <footer>
+        <h5><a href= "Link here"> Link to the GitHub Repository Code </a><h5>
 </body>
 </html>
