7b4ea576e2df39a45d0129d0489c8e3225293198
TalhaShaukatAli <tr7251ub@go.minnstate.edu>
2024-03-07 12:17:45 -0600

    Added projects view

--- src/views/navigation.view.php
+++ src/views/navigation.view.php
@@ -3,6 +3,7 @@
         <ul>
             <li> <a href="<?php echo URL_ROOT ?>"> Home</a></li>
             <li> <a href="<?php echo URL_ROOT . "/articles/assignments.php"?>"> Assignments</a></li>
+            <li> <a href="<?php echo URL_ROOT . "/articles/projects.php"?>"> Projects</a></li>
             <li> <a href="<?php echo URL_ROOT . "/articles/about.php"?>"> About</a></li>
         </ul>
     </nav>
